7221faca95592f6e9e4e248582fba7b37dfc6a0f
Jon Godin <jon.m.godin@gmail.com>
2026-02-21 22:23:38 -0800

    Conform icons

--- sesh/icons.sh
+++ sesh/icons.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # Central icon definitions for sesh session categories.
 # Sourced by build_sesh_config.sh, wt-helpers.sh, and .zshrc.
+#
+# Icons use hex escapes to prevent silent corruption by editors/tools.
 
-ICON_TOOL="󱘖"
-ICON_CONFIG="󱁤"
-ICON_PROJECT=" "
-ICON_WORKTREE="󰀜"
-ICON_WORKTREE_PROJECT="󱁤"
+ICON_TOOL=$'\xf3\xb1\x98\x96'              # U+F1616 󱘖
+ICON_CONFIG=$'\xf3\xb1\x81\xa4'             # U+F1064 󱁤
+ICON_PROJECT=$'\xee\x9f\x85'                # U+E7C5
+ICON_WORKTREE=$'\xf3\xb0\x80\x9c'           # U+F001C 󰀜
+ICON_WORKTREE_PROJECT=$'\xf3\xb1\x81\xa4'   # U+F1064 󱁤
