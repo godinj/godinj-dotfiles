49e1288e0c842a8574947d9628dd6140a7676b88
Jon Godin <jon.m.godin@gmail.com>
2026-05-02 20:34:55 -0700

    Replace render-markdown with markview

---
+++ nvim/lua/custom/plugins/markview.lua
@@ -0,0 +1,15 @@
+return {
+  'OXY2DEV/markview.nvim',
+  lazy = false,
+  opts = {
+    markdown = {
+      list_items = {
+        marker_minus = { add_padding = false },
+        marker_plus = { add_padding = false },
+        marker_star = { add_padding = false },
+        marker_dot = { add_padding = false },
+        marker_parenthesis = { add_padding = false },
+      },
+    },
+  },
+}
--- nvim/lua/custom/plugins/render-markdown.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  opts = {
-    anti_conceal = { enabled = false },
-  },
-  keys = {
-    { '<leader>tm', '<cmd>RenderMarkdown toggle<cr>', desc = '[T]oggle [M]arkdown render' },
-  },
-}
